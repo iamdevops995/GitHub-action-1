a20fb6da63ae9d71254ccf7690c38ce6673b07a4
logeshkumar <iamdevops995@gmail.com>
2025-11-25 12:36:23 +0000

    Add new decryption script

---
+++ decrypt_secret.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/my_secret.json my_secret.json.gpg
+#
